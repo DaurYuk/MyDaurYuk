a0874d1f5800de8e39d8f9795e5d2e9c292bb20b
izalfront <rizalhajakubastarus@gmail.com>
2024-06-10 22:51:17 +0800

    build feature reward 75% & prepare API registration. login 50%

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("kotlin-parcelize")
+    id("kotlin-kapt")
 }
 
 android {
@@ -47,30 +48,67 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.github.bumptech.glide:glide:4.13.2")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.paging:paging-runtime-ktx:3.1.1")
+    implementation("androidx.room:room-paging:2.5.1")
+    implementation("androidx.room:room-runtime:2.5.1")
+    kapt("androidx.room:room-compiler:2.5.1")
+    implementation("androidx.room:room-ktx:2.5.1")
+    implementation("com.github.bumptech.glide:glide:4.15.0")
 
-    // Room
-    implementation("androidx.room:room-runtime:2.6.0")
-    ksp("androidx.room:room-compiler:2.6.0")
-    implementation("androidx.room:room-ktx:2.6.0")
-
-    // Retrofit
+    implementation("id.zelory:compressor:3.0.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.5")
 
-    // Coroutine support
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
 
+    testImplementation("org.mockito:mockito-core:3.12.4")
+    testImplementation("org.mockito:mockito-inline:3.12.4")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
+    testImplementation("io.strikt:strikt-core:0.31.0")
+
+    androidTestImplementation("com.squareup.okhttp3:mockwebserver3:5.0.0-alpha.2")
+    androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
+
+    debugImplementation("androidx.fragment:fragment-testing:1.4.1")
+    implementation("androidx.test.espresso:espresso-idling-resource:3.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.4.0")
+
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.android.gms:play-services-places:17.1.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    // Unit testing dependencies
+    testImplementation ("junit:junit:4.13.2")
+    testImplementation ("org.mockito:mockito-core:3.9.0")
+    testImplementation ("androidx.arch.core:core-testing:2.1.0")
+    testImplementation ("androidx.paging:paging-common:3.1.0")
+
+    // Coroutine dependencies for testing
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
+
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    testImplementation ("androidx.arch.core:core-testing:2.1.0")
+
+    // DataStore
+    implementation("androidx.datastore:datastore-core-android:1.1.1")
+
+    // ViewPager2
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
@@ -82,8 +120,9 @@ dependencies {
 
     // Paging 3
     val pagingVersion = "3.0.0"
-    implementation ("androidx.paging:paging-runtime:$pagingVersion")
+    implementation("androidx.paging:paging-runtime:$pagingVersion")
 
     // Authentication Google Account
-    implementation ("com.google.android.gms:play-services-auth:19.0.0")
+    implementation("com.google.android.gms:play-services-auth:19.0.0")
 }
+
--- app/src/main/java/com/example/mycapstone/register_login/viewmodel/RegisterDataAccount.kt
+++ app/src/main/java/com/example/mycapstone/register_login/viewmodel/RegisterDataAccount.kt
@@ -1,2 +1,29 @@
 package com.example.mycapstone.register_login.viewmodel
 
+data class RegisterDataAccount(
+    var name: String,
+    var email: String,
+    var password: String
+)
+
+data class LoginDataAccount(
+    var email: String,
+    var password: String
+)
+
+data class ResponseDetail(
+    var error: Boolean,
+    var message: String
+)
+
+data class ResponseLogin(
+    var error: Boolean,
+    var message: String,
+    var loginResult: LoginResult
+)
+
+data class LoginResult(
+    var userId: String,
+    var name: String,
+    var token: String
+)
